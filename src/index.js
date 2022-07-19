edd6791df1a4c4d21a48740103ebf6995e41c84c
Andron67 <zizin.a@mail.ru>
2022-07-19 15:05:26 +0200

    Add changes

--- src/index.js
+++ src/index.js
@@ -28,8 +28,8 @@ var swiper = new Swiper(".offerSwiper", {
 		},
 
 		768: {
-			slidesPerView: 2,
-			slidesPerGroup: 2
+			slidesPerView: 1,
+			slidesPerGroup: 1
 		},
 
 		1000: {
@@ -69,5 +69,38 @@ var swiper = new Swiper(".offerSwiper", {
 			slidesPerView: 3,
 			slidesPerGroup: 3
 		 }
+	}
+ });
+
+ var swiper = new Swiper(".catSwiper", {
+	slidesPerView: 3,
+	spaceBetween: 30,
+	slidesPerGroup: 3,
+	loop: true,
+	loopFillGroupWithBlank: true,
+	pagination: {
+	  el: ".swiper-pagination",
+	  clickable: true,
+	},
+	navigation: {
+	  nextEl: ".swiper-button-next",
+	  prevEl: ".swiper-button-prev",
+	},
+	modules: [Navigation, Pagination],
+	breakpoints: {
+		320: {
+		  slidesPerView: 1,
+		  slidesPerGroup: 1
+		},
+
+		768: {
+			slidesPerView: 1,
+			slidesPerGroup: 1
+		},
+
+		1000: {
+			slidesPerView: 3,
+			slidesPerGroup: 3
+		}
 	}
  });
