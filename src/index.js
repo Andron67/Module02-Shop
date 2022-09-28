4ba8661a066f3655804699488fd19bdec6451087
Andron67 <zizin.a@mail.ru>
2022-09-28 21:57:30 +0200

    Add changes

--- src/index.js
+++ src/index.js
@@ -78,10 +78,6 @@ var swiper = new Swiper(".offerSwiper", {
 	slidesPerGroup: 3,
 	loop: true,
 	loopFillGroupWithBlank: true,
-	pagination: {
-	  el: ".swiper-pagination",
-	  clickable: true,
-	},
 	navigation: {
 		nextEl: ".swiper-button-next",
 		prevEl: ".swiper-button-prev",
